c95434395e90981f43af7313d97e2508e8c967e8
Tang <tangllty@163.com>
2022-04-18 14:49:36 +0800

    初始化

---
+++ gulimall-product/src/test/java/com/tang/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
